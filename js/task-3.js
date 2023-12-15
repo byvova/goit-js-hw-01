152f5a60846f25d59f04f5b48dfed8ced3860608
blyavova <grushvo3@gmail.com>
2023-12-15 20:52:43 +0200

    update

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,9 @@
 function getElementWidth(content, padding, border) {
-    const contentWidth = parseFloat(content); 
-    const paddingWidth = parseFloat(padding);
-    const borderWidth = parseFloat(border);
+    const contentWidth = Number.parseFloat(content);
+    const paddingWidth = 2 * Number.parseFloat(padding);
+    const borderWidth = 2 * Number.parseFloat(border);
     return contentWidth + paddingWidth + borderWidth;
 }
-
 console.log(getElementWidth("50px", "8px", "4px")); 
 console.log(getElementWidth("60px", "12px", "8.5px")); 
 console.log(getElementWidth("200px", "0px", "0px")); 
